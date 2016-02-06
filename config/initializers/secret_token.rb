c22e2d79ecb33ce2d956d4d678e748066f495864
Francis Aguero <motionborg.station1@gmail.com>
2016-02-06 08:00:39 -0430

    Creado el proyecto y agregado status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tutorialbook::Application.config.secret_token = '493bd5e67c4cd50c0543738bdcf0b849ed3aa40267a7eebe544a04ce68584c0651adb537a5f6917868aef0fd843040ec6fc995318bf777d34762e774665792d8'
